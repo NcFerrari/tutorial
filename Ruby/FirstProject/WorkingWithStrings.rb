88d05fcbb5e79976117af6a9d246504cc9ccc1d6
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-11-06 15:25:28 +0100

    user input + text format

--- Ruby/FirstProject/WorkingWithStrings.rb
+++ Ruby/FirstProject/WorkingWithStrings.rb
@@ -14,3 +14,15 @@ puts "spojování" + " řetězců"
 puts "klasika".upcase()
 
 puts "pokud chceme vložit číslo do výpisu, musíme ho přetypovat (třeba jako: " + 10.to_s + ")"
+
+formatted_string = sprintf("formátovaný text %s", "jako v Javičce")
+puts formatted_string
+
+formatted_string = "dá se to psát zkratkou bez sprintf metody %s. Toto je vcelku dobré" %["jako třeba takto"]
+puts formatted_string
+
+formatted_string = "funguje i pro čísla (%d) samozřejmě" %[321]
+puts formatted_string
+
+formatted_string = "desetinná čísla lze psát klasicky jako třeba %.2f" %[Math::PI]
+puts formatted_string
