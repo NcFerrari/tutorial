45f623a38fdacbe000aff6e3824eaf4c91a10d00
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-11-07 09:17:55 +0100

    array and map

---
+++ Ruby/FirstProject/10_hashes_neboli_mapy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+states = {
+  "PA" => "Pennsylvania",
+  "NY" => "New York",
+  "OR" => "Oregon"
+}
+
+puts states["PA"]
+
+puts states
+puts states.keys
+puts states.values
