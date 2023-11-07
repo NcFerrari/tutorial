f3ef9c6a6e21ecfc9181aff8fc44a5072f9e891f
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-11-07 08:53:51 +0100

    Simple example

---
+++ Ruby/FirstProject/Kalkulacka.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+puts "Zadej první číslo:"
+first_number = gets.chomp().to_f
+puts "Zadej druhé číslo:"
+second_number = gets.chomp().to_f
+puts "zadej operaci"
+operation = gets().chomp()
+
+case operation
+when "+"
+  puts first_number + second_number
+when "-"
+  puts first_number - second_number
+when "*"
+  puts first_number * second_number
+when "/"
+  puts first_number / second_number
+when "%"
+  puts first_number % second_number
+when "sqrt"
+  puts Math.sqrt(first_number)
+when "pow"
+  puts first_number ** second_number
+
+end
