a69d16eadc8d7751d4ef4a9cd555f2ee64177a2c
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-11-24 15:51:46 +0100

    funkční varianta

--- Ruby/rubydev/kap05/vazbawebuamodelu/app/controllers/hele_controller.rb
+++ Ruby/rubydev/kap05/vazbawebuamodelu/app/controllers/hele_controller.rb
@@ -1,4 +1,9 @@
 class HeleController < ApplicationController
+
   def tady
+    zpracovatel = Zpracovatel.new
+    zpracovatel.data = params[:zpracovatel][:data]
+    puts zpracovatel.data
   end
+
 end
---
+++ Ruby/rubydev/kap05/vazbawebuamodelu/app/models/zpracovatel.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Zpracovatel
+  attr_accessor :data
+
+end
