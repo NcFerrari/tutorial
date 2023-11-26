441b9b97e62646c2f07135411d6b001d76d2ee22
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-11-26 06:40:44 +0100

    reverd changes

--- Ruby/rubydev/kap05/vazbamodeluaaplikace/app/controllers/hele_controller.rb
+++ Ruby/rubydev/kap05/vazbamodeluaaplikace/app/controllers/hele_controller.rb
@@ -1,10 +1,11 @@
 class HeleController < ApplicationController
   def tady
-    @data_asoc = params[:objekt]
-    @zpracovatel = Zpracovatel.new @data_asoc[:metoda]
+    @data_asoc = params[:zpracovatel]
+    @zpracovatel = Zpracovatel.new @data_asoc[:zpracovat]
     @data = @zpracovatel.zpracovat
   end
 
   def vstup
+    @zpracovatel = Zpracovatel.new "Gagarencis"
   end
 end
