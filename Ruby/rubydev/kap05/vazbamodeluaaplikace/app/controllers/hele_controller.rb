dbc03178925c531f56d58410879923b970e9b4c0
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-11-26 07:24:56 +0100

    excercise 5.3

--- Ruby/rubydev/kap05/vazbamodeluaaplikace/app/controllers/hele_controller.rb
+++ Ruby/rubydev/kap05/vazbamodeluaaplikace/app/controllers/hele_controller.rb
@@ -1,11 +1,12 @@
 class HeleController < ApplicationController
   def tady
     @data_asoc = params[:zpracovatel]
-    @zpracovatel = Zpracovatel.new @data_asoc[:zpracovat]
+    @zpracovatel = Zpracovatel.new @data_asoc[:zpracovat], @data_asoc[:vek]
     @data = @zpracovatel.zpracovat
+    @vek = @zpracovatel.vek
   end
 
   def vstup
-    @zpracovatel = Zpracovatel.new "Gagarencis"
+    @zpracovatel = Zpracovatel.new "Gagarencis", 1024
   end
 end
--- Ruby/rubydev/kap05/vazbamodeluaaplikace/app/models/zpracovatel.rb
+++ Ruby/rubydev/kap05/vazbamodeluaaplikace/app/models/zpracovatel.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Zpracovatel
-  attr_reader :zpracovat
-  attr_writer :zpracovat
+  attr_accessor :zpracovat
+  attr_accessor :vek
 
-  def initialize data
+  def initialize data, vek
     @zpracovat = data
+    @vek = vek
   end
 end
