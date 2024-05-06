cd6987b22b9ab9c8b7fe4166fc3d3388d40962df
Lubor Pešek <lubor.pesek@qinshift.com>
2024-05-06 09:34:12 +0200

    Sonar refactor

--- Java/JDBCtutorial/src/main/java/lp/be/jpa/db/DBConnection.java
+++ Java/JDBCtutorial/src/main/java/lp/be/jpa/db/DBConnection.java
@@ -2,6 +2,7 @@ package lp.be.jpa.db;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.mysql.cj.util.StringUtils;
 import generator.Human;
 import generator.utils.HumanAtr;
 import lp.be.service.LoggerService;
@@ -33,13 +34,13 @@ public class DBConnection {
     }
 
     private void runMethods() throws SQLException {
-        newPeopleCount = 0;
-//        create();
-//        read();
-//        update();
-//        delete();
-//        preparedStatementExample();
-//        callableStatementExample();
+        newPeopleCount = 10;
+        create();
+        read();
+        update();
+        delete();
+        preparedStatementExample();
+        callableStatementExample();
         greet();
     }
 
@@ -88,10 +89,10 @@ public class DBConnection {
 
     private void read() throws SQLException {
         log.info("READ FROM DATABASE");
-        ResultSet myRs = statement.executeQuery("SELECT * FROM employees");
-        while (myRs.next()) {
-            String stringBuilder = myRs.getString("last_name") + "," + myRs.getString("first_name");
-            log.info(stringBuilder);
+        ResultSet rs = statement.executeQuery("SELECT * FROM employees");
+        while (rs.next()) {
+            String result = getBasicData(rs);
+            log.info(result);
         }
     }
 
@@ -108,12 +109,13 @@ public class DBConnection {
     }
 
     private void preparedStatementExample() {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employees WHERE first_name LIKE ?")) {
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM employees WHERE first_name LIKE ?")) {
             preparedStatement.setString(1, "%a%");
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
-                String stringBuilder = rs.getString("last_name") + "," + rs.getString("first_name");
-                log.info(stringBuilder);
+                String result = getBasicData(rs);
+                log.info(result);
             }
         } catch (SQLException e) {
             log.error(e.getMessage());
@@ -125,47 +127,45 @@ public class DBConnection {
         int theIncreaseAmount = 10_000;
         log.info("Salaries BEFORE");
         log.info("");
-        ResultSet myRs = statement.executeQuery("SELECT * FROM employees WHERE department='" + theDepartment + "'");
-        while (myRs.next()) {
-            String stringBuilder = myRs.getString("last_name") + "," + myRs.getString("first_name") + ", " + myRs.getString("department") + ", " + myRs.getString("salary");
-            log.info(stringBuilder);
-        }
-        try (CallableStatement callableStatement = connection.prepareCall("{call increase_salaries_for_department(?, ?)}")) {
-            callableStatement.setString(1, theDepartment);
-            callableStatement.setDouble(2, theIncreaseAmount);
+        testingOutput(theDepartment);
+        try (CallableStatement cs = connection.prepareCall("{call increase_salaries_for_department(?, ?)}")) {
+            cs.setString(1, theDepartment);
+            cs.setDouble(2, theIncreaseAmount);
 
             log.info("");
             log.info("");
-            log.info("Calling stored procedure. increase_salaries_for_department('{}', {})", theDepartment, theIncreaseAmount);
-            callableStatement.execute();
+            log.info("Calling stored procedure. increase_salaries_for_department('{}', {})",
+                    theDepartment, theIncreaseAmount);
+            cs.execute();
             log.info("Finished calling stored procedure");
             log.info("");
             log.info("");
             log.info("Salaries AFTER");
             log.info("");
         }
-        myRs = statement.executeQuery("SELECT * FROM employees WHERE department='" + theDepartment + "'");
-        while (myRs.next()) {
-            String stringBuilder = myRs.getString("last_name") + "," + myRs.getString("first_name") + ", " + myRs.getString("department") + ", " + myRs.getString("salary");
-            log.info(stringBuilder);
-        }
+        testingOutput(theDepartment);
     }
 
     private void greet() throws SQLException {
         String theDepartment = "Engineering";
-        ResultSet myRs = statement.executeQuery("SELECT * FROM employees WHERE department='" + theDepartment + "'");
-        while (myRs.next()) {
-            String stringBuilder = myRs.getString("last_name") + "," + myRs.getString("first_name") + ", " + myRs.getString("department") + ", " + myRs.getString("salary");
-            log.info(stringBuilder);
-        }
+        testingOutput(theDepartment);
         try (CallableStatement callableStatement = connection.prepareCall("{call greet_the_department(?)}")) {
             callableStatement.setString(1, theDepartment);
             callableStatement.execute();
         }
-        myRs = statement.executeQuery("SELECT * FROM employees WHERE department='" + theDepartment + "'");
-        while (myRs.next()) {
-            String stringBuilder = myRs.getString("last_name") + "," + myRs.getString("first_name") + ", " + myRs.getString("department") + ", " + myRs.getString("salary");
+        testingOutput(theDepartment);
+    }
+
+    private void testingOutput(String theDepartment) throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT * FROM employees WHERE department='" + theDepartment + "'");
+        while (rs.next()) {
+            String stringBuilder = getBasicData(rs) + ", " + rs.getString("department") + ", " + rs.getString("salary");
             log.info(stringBuilder);
         }
+    }
+
+    private String getBasicData(ResultSet rs) throws SQLException {
+        String result = rs.getString("last_name") + "," + rs.getString("first_name");
+        return StringUtils.isNullOrEmpty(result) ? "Nothing to display" : result;
     }
 }
