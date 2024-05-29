8272cbcdf415ad9560944954e7a56840bae7796f
Lubor Pešek <lubor.pesek@qinshift.com>
2024-05-29 13:34:58 +0200

    outputs

--- python/w3school/pythonTutorial/src/$3_variables.py
+++ python/w3school/pythonTutorial/src/$3_variables.py
@@ -1,4 +1,4 @@
-def fileChooser(case):
+def file_chooser(case):
     match case:
         case 1:
             return "$1_basics"
@@ -7,14 +7,14 @@ def fileChooser(case):
         case 3:
             return "$3_multiple_values"
         case 4:
-            return "$4_"
+            return "$4_output"
         case 5:
             return "$5_"
         case 6:
             return "$6_"
 
 
-fileName = "variables/" + fileChooser(3) + ".py"
+fileName = "variables/" + file_chooser(4) + ".py"
 
 with open(fileName, encoding="utf-8") as file:
     exec(file.read())
