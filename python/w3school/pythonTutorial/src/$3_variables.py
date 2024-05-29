68532df1bccfbbc368da087ae95bb2676274e007
Lubor Pešek <lubor.pesek@qinshift.com>
2024-05-29 12:31:02 +0200

    changed project structure

--- python/w3school/pythonTutorial/src/$3_variables.py
+++ python/w3school/pythonTutorial/src/$3_variables.py
@@ -1,4 +1,20 @@
-x = 5
-y = "Achoj prdi!"
-print(x)
-print(y)
+def fileChooser(case):
+    match case:
+        case 1:
+            return "$1_basics"
+        case 2:
+            return "$2_names"
+        case 3:
+            return "$3_"
+        case 4:
+            return "$4_"
+        case 5:
+            return "$5_"
+        case 6:
+            return "$6_"
+
+
+fileName = "variables/" + fileChooser(2) + ".py"
+
+with open(fileName) as file:
+    exec(file.read())
