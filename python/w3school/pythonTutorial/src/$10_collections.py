9a45f75b5c3c1f8226a76ca6f8121ad9fe339969
Lubor Pešek <lubor.pesek@qinshift.com>
2024-05-30 13:16:18 +0200

    map

--- python/w3school/pythonTutorial/src/$10_collections.py
+++ python/w3school/pythonTutorial/src/$10_collections.py
@@ -6,9 +6,11 @@ def file_chooser(case):
             return "$2_loops"
         case 3:
             return "$3_set"
+        case 4:
+            return "$4_mapa_alias_dictionary"
 
 
-fileName = "collections/" + file_chooser(3) + ".py"
+fileName = "collections/" + file_chooser(4) + ".py"
 
 with open(fileName, encoding="utf-8") as file:
     exec(file.read())
