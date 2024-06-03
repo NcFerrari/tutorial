392c7784d5a4bb4581a5de7551ffeefb66841c78
Lubor Pešek <lubor.pesek@qinshift.com>
2024-06-03 14:30:27 +0200

    matematické funkce

---
+++ python/w3school/pythonTutorial/src/$16_math.py
@@ -0,0 +1,17 @@
+import random
+import math
+
+newArray = []
+for i in range(0, 100):
+    newArray.append(random.randint(0, 100))
+
+print(min(newArray))
+print(max(newArray))
+print(abs(-5))
+print(pow(7, 3))
+
+print(math.sqrt(9))
+print(math.ceil(65.45676878))
+print(math.floor(65.45676878))
+print(math.pi)
+
