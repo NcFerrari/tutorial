488811a18fc9fa325bb78b7cae17f874f9e67dc0
Lubor Pešek <lubor.pesek@qinshift.com>
2024-08-05 13:00:13 +0200

    used data for services

--- JavaScript/Udemy/React/backroads-app/src/components/Services.js
+++ JavaScript/Udemy/React/backroads-app/src/components/Services.js
@@ -1,42 +1,23 @@
 import Title from "./Title";
+import {service} from "../data"
 
 const Services = () => {
     return (
         <section className="section services" id="services">
             <Title firstName={"our"} secondName={"services"}/>
             <div className="section-center services-center">
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
-                    <div className="service-info">
-                        <h4 className="service-title">saving money</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
-                    <div className="service-info">
-                        <h4 className="service-title">endless hiking</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-                    <div className="service-info">
-                        <h4 className="service-title">amazing comfort</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
+                {service.map((link) => {
+                    return (
+                        <article className="service" key={link.id}>
+                            <span className="service-icon"><i className={link.icon}></i></span>
+                            <div className="service-info">
+                                <h4 className="service-title">{link.title}</h4>
+                                <p className="service-text">{link.text}
+                                </p>
+                            </div>
+                        </article>
+                    )
+                })}
             </div>
         </section>
     )
--- JavaScript/Udemy/React/backroads-app/src/data.js
+++ JavaScript/Udemy/React/backroads-app/src/data.js
@@ -26,3 +26,24 @@ export const media = [
     {href: "https://www.twitter.com", icon: "fab fa-twitter"},
     {href: "https://www.squarespace.com", icon: "fab fa-squarespace"}
 ]
+
+export const service = [
+    {
+        id: 0,
+        icon: "fas fa-wallet fa-fw",
+        title: "saving money",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    },
+    {
+        id: 1,
+        icon: "fas fa-tree fa-fw",
+        title: "endless hiking",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    },
+    {
+        id: 2,
+        icon: "fas fa-socks fa-fw",
+        title: "amazing comfort",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    }
+]
