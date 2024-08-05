5808156054d38c9e43eb50895621a64a0c1d42b1
Lubor Pešek <lubor.pesek@qinshift.com>
2024-08-05 11:48:51 +0200

    changed menu to reference it from one place

---
+++ JavaScript/Udemy/React/backroads-app/src/data.js
@@ -0,0 +1,22 @@
+export const pageLinks = [
+    {
+        id: 0,
+        href: "#home",
+        text: "home"
+    },
+    {
+        id: 1,
+        href: "#about",
+        text: "about"
+    },
+    {
+        id: 2,
+        href: "#services",
+        text: "services"
+    },
+    {
+        id: 3,
+        href: "#tours",
+        text: "tours"
+    },
+]
