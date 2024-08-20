d895921031c0c940f9e4af48b938ca55f146246d
Lubor Pešek <lubor.pesek@qinshift.com>
2024-08-20 11:59:04 +0200

    Array hook

--- JavaScript/Udemy/React/vite_project/src/App.jsx
+++ JavaScript/Udemy/React/vite_project/src/App.jsx
@@ -1,8 +1,11 @@
-import Starter from "./tutorial/01-useState/starter/02-useState-basics.jsx"
+import Starter from "./tutorial/01-useState/starter/03-useState-array.jsx"
+import Final from "./tutorial/01-useState/final/03-useState-array.jsx"
 
 function App() {
     return <div className={"container"}>
         <Starter/>
+        <hr/>
+        <Final/>
     </div>
 }
 
--- JavaScript/Udemy/React/vite_project/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ JavaScript/Udemy/React/vite_project/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,50 @@
+import {data} from "../../../data.js";
+import React, {useEffect} from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+
+    const [people, setPeople] = React.useState([]);
+
+    const removing = (id) => {
+        setPeople(people.filter((person) => person.id !== id))
+    }
+
+    function clearItems() {
+        setPeople([])
+    }
+
+    function resetItems() {
+        setPeople(data)
+    }
+
+    useEffect(() => {
+        resetItems();
+    }, []);
+
+    return <div>
+        {people.map((person) => {
+            const {id, name} = person
+            return <div key={id}>
+                <h4>{name}</h4>
+                <button
+                    onClick={() => removing(id)}>
+                    remove
+                </button>
+            </div>
+        })}
+        <button
+            style={{marginTop: "2rem"}}
+            className={"btn"}
+            onClick={clearItems}>
+            clear items
+        </button>
+        <button
+            style={{marginLeft: "2rem"}}
+            className={"btn"}
+            onClick={resetItems}>
+            reset items
+        </button>
+    </div>
+}
 
 export default UseStateArray;
