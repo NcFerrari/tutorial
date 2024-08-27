207fa3d7c79ae5226771e4624ca7bc79c273b6cb
Lubor Pešek <lubor.pesek@qinshift.com>
2024-08-27 08:46:13 +0200

    useEffect continue

--- JavaScript/Udemy/React/vite_project/src/App.jsx
+++ JavaScript/Udemy/React/vite_project/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from "./tutorial/02-useEffect/starter/01-code-example.jsx"
-import Final from "./tutorial/02-useEffect/final/01-code-example.jsx"
+import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects.jsx"
+import Final from "./tutorial/02-useEffect/final/03-multiple-effects.jsx"
 
 function App() {
     return <div className={"container"}>
