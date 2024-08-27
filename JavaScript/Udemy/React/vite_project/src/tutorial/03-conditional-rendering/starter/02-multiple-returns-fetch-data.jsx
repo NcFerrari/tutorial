fcb5cdf3320faaba92ebe1a5995d3de985ff2942
Lubor Pešek <lubor.pesek@qinshift.com>
2024-08-27 14:18:44 +0200

    refactoring previous fetch data code

--- JavaScript/Udemy/React/vite_project/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ JavaScript/Udemy/React/vite_project/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -6,11 +6,17 @@ const MultipleReturnsFetchData = () => {
 
     const [loading, setLoading] = useState(true)
     const [user, setUser] = useState(null);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const resp = await fetch(url);
+                if (!resp.ok) {
+                    setIsError(true);
+                    setLoading(false)
+                    return
+                }
                 const user = await resp.json();
                 setUser(user)
                 setLoading(false)
@@ -22,7 +28,9 @@ const MultipleReturnsFetchData = () => {
     }, [])
 
     if (loading) {
-        return <div>Loading...</div>
+        return <h2>Loading...</h2>
+    } else if (isError) {
+        return <h2>there was an error...</h2>
     }
     return <div>
         <img style={{width: "150px", borderRadius: "25px"}} src={user.avatar_url} alt={user.name}/>
