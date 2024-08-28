56560977a809ce509ddb215b8b5e593722c52cd6
Lubor Pešek <lubor.pesek@qinshift.com>
2024-08-28 10:17:09 +0200

    Ternary operator and conditions in react rendering

--- JavaScript/Udemy/React/vite_project/src/App.jsx
+++ JavaScript/Udemy/React/vite_project/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx"
-import Final from "./tutorial/03-conditional-rendering/final/04-short-circuit-overview.jsx"
+import Starter from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx"
+import Final from "./tutorial/03-conditional-rendering/final/05-short-circuit-examples.jsx"
 
 function App() {
     return <div className={"container"}>
--- JavaScript/Udemy/React/vite_project/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ JavaScript/Udemy/React/vite_project/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 const ShortCircuitExamples = () => {
-  // falsy
-  const [text, setText] = useState('');
-  // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
-  const [isEditing, setIsEditing] = useState(false);
+    // falsy
+    const [text, setText] = useState('');
+    // truthy
+    const [name, setName] = useState('susan');
+    const [user, setUser] = useState({name: "john"});
+    const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+    return <div>
+        <h2>{text || "default value"}</h2>
+        {user && <UserComponent name={user.name}/>}
+        <div>
+            <h2 style={{margin: "1rem 0"}}>Ternary Operator</h2>
+            <button className={"btn"}>{isEditing ? "edit" : "add"}</button>
+            {user ? <div>
+                <h4>hello there user {user.name}</h4>
+            </div> : <div><h2>please login</h2></div>}
+        </div>
+    </div>
 };
+
+const UserComponent = ({name}) => {
+    return <div>
+        <h2>whatever return</h2>
+        <h2>{name}</h2>
+    </div>
+}
+
+const TernaryComponent = () => {
+    return
+}
 
 export default ShortCircuitExamples;
