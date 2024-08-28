de050bd21560854ab05235b8a14e51d0286c31e1
Lubor Pešek <lubor.pesek@qinshift.com>
2024-08-28 14:52:02 +0200

    Project structure by react

--- JavaScript/Udemy/React/vite_project/src/App.jsx
+++ JavaScript/Udemy/React/vite_project/src/App.jsx
@@ -1,11 +1,8 @@
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function.jsx"
-import Final from "./tutorial/02-useEffect/final/05-cleanup-function.jsx"
+import Example from "./tutorial/04-project-structure/starter/example";
 
 function App() {
     return <div className={"container"}>
-        <Starter/>
-        <hr/>
-        <Final/>
+        <Example/>
     </div>
 }
 
---
+++ JavaScript/Udemy/React/vite_project/src/tutorial/04-project-structure/starter/example/SecondComponent.jsx
@@ -0,0 +1,5 @@
+const SecondComponent = () => {
+    return <div><h2>Second Component</h2></div>
+}
+
+export default SecondComponent
