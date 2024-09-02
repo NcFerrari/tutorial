c2f532059c6dbbd584563722d46fb1c73ff6ae48
Lubor Pešek <lubor.pesek@qinshift.com>
2024-09-02 14:48:28 +0200

    Refactoring (setting default value instead of condition)

--- JavaScript/Udemy/React/vite_project/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ JavaScript/Udemy/React/vite_project/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,9 +1,11 @@
-const Person = ({name, nickName = "shakeAndBake", images}) => {
-
-    const img = images?.[0]?.small?.url || "/src/assets/default-avatar.svg";
+const Person = ({
+                    name = "undefined",
+                    nickName = "shakeAndBake",
+                    images = [{small: {url: "/src/assets/default-avatar.svg"}}]
+                }) => {
 
     return <div>
-        <img src={img} alt={name} style={{width: "50px"}}/>
+        <img src={images[0].small.url} alt={name} style={{width: "50px"}}/>
         <h4>{name}</h4>
         <p>Nickname : {nickName}</p>
     </div>
