3d26655db30aec84bc82561d10ab88c5935ba58d
Lubor <lubor.pesek@post.cz>
2024-09-11 12:41:42 +0200

    useRef hook

--- JavaScript/Udemy/React/vite_project/src/App.jsx
+++ JavaScript/Udemy/React/vite_project/src/App.jsx
@@ -1,5 +1,5 @@
-import Start from "./tutorial/06-forms/starter/05-form-data.jsx"
-import Final from "./tutorial/06-forms/final/05-form-data.jsx"
+import Start from "./tutorial/07-useRef/starter/01-useRef-basics.jsx"
+import Final from "./tutorial/07-useRef/final/01-useRef-basics.jsx"
 
 
 function App() {
