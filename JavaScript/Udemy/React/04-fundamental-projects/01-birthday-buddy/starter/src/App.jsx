b3f12aef8a73c5fc56809eac44d7ec2003023d2e
Lubor Pešek <lubor.pesek@qinshift.com>
2024-09-12 09:51:09 +0200

    Basic setup of project

--- JavaScript/Udemy/React/04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ JavaScript/Udemy/React/04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,28 @@
+import data from "./data.js"
+
 const App = () => {
-    return <div></div>
+
+    const getBirthdaysCount = () => {
+        const count = data.length;
+        return count > 1 ? count + " birthdays" : (count === 0 ? "no birthday" : count + " birthday");
+    }
+
+    return <div>
+        <h2>{getBirthdaysCount()} today</h2>
+        {data.map((personData) => {
+            return <div key={personData.id}>
+                <Person {...personData}/>
+            </div>
+        })}
+        <button className={"btn btn-block"}>clear all</button>
+    </div>
 };
+
+const Person = ({name, age, image}) => {
+    return <div>
+        <img src={image} style={{width: "56px", height: "56px"}}/>
+        <h5>{name}</h5>
+        <p>{age} {age > 1 ? "years" : "year"}</p>
+    </div>
+}
 export default App;
